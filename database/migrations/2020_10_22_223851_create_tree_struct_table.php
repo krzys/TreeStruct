3e595dbcc02a6b06c9da136165810234ed3c6c75
Krzysztof Sternik <sternik15@gmail.com>
2020-11-01 22:05:40 +0100

    Create stored procedure for changing display order of tree_struct rows

--- database/migrations/2020_10_22_223851_create_tree_struct_table.php
+++ database/migrations/2020_10_22_223851_create_tree_struct_table.php
@@ -19,6 +19,21 @@ class CreateTreeStructTable extends Migration
             $table->unsignedInteger('parent')->nullable();
             $table->unsignedInteger('display_order')->default(0);
         });
+
+        \Illuminate\Support\Facades\DB::raw('' .
+        'DELIMITER ;;
+        CREATE PROCEDURE `change_tree_struct_element_order` (IN idx int, IN ord int)
+        BEGIN
+            SET @parent = (SELECT parent FROM tree_struct WHERE id = idx);
+            UPDATE tree_struct SET display_order = ord WHERE id = idx;
+
+            SET @row = 0;
+            UPDATE tree_struct as `a` SET display_order = (SELECT r FROM (SELECT (@row:=@row+1) as r, id FROM tree_struct WHERE parent = @parent ORDER BY display_order) as `b` WHERE b.id = a.id) WHERE parent = @parent AND id != idx;
+
+            SET @duplicatedId = (SELECT id FROM tree_struct WHERE display_order = ord AND id != idx LIMIT 1);
+            UPDATE tree_struct SET display_order = ord + 1 WHERE id = @duplicatedId;
+        END;;
+        DELIMITER ;');
     }
 
     /**
@@ -29,5 +44,6 @@ class CreateTreeStructTable extends Migration
     public function down()
     {
         Schema::dropIfExists('tree_struct');
+        \Illuminate\Support\Facades\DB::raw('DROP PROCEDURE IF EXISTS `change_tree_struct_element_order`;');
     }
 }
