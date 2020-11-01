151f0a74ddceb60392f170e97ddc6d1ea29b15d5
Krzysztof Sternik <sternik15@gmail.com>
2020-11-02 00:59:56 +0100

    Fix stored procedure

--- database/migrations/2020_10_22_223851_create_tree_struct_table.php
+++ database/migrations/2020_10_22_223851_create_tree_struct_table.php
@@ -25,14 +25,13 @@ class CreateTreeStructTable extends Migration
         CREATE PROCEDURE `change_tree_struct_element_order` (IN idx int, IN ord int)
         BEGIN
             SET @parent = (SELECT parent FROM tree_struct WHERE id = idx);
+            SET @row = 0;
+            UPDATE tree_struct as `a` SET display_order = (SELECT r FROM (SELECT (@row:=@row+1) as r, id FROM tree_struct WHERE ((@parent is null and parent is null) OR parent = @parent) ORDER BY display_order) as `b` WHERE b.id = a.id) WHERE ((@parent is null and parent is null) OR parent = @parent);
+            SET @prevValue = (SELECT display_order FROM tree_struct WHERE id = idx);
             UPDATE tree_struct SET display_order = ord WHERE id = idx;
 
-            SET @row = 0;
-            UPDATE tree_struct as `a` SET display_order = (SELECT r FROM (SELECT (@row:=@row+1) as r, id FROM tree_struct WHERE ((@parent is null and parent is null) OR parent = @parent) ORDER BY display_order) as `b` WHERE b.id = a.id) WHERE ((@parent is null and parent is null) OR parent = @parent) AND id != idx;
-
-            SET @duplicatedId = (SELECT id FROM tree_struct WHERE display_order = ord AND id != idx LIMIT 1);
-            SET @count = (SELECT COUNT(*) FROM tree_struct WHERE (@parent is null and parent is null) or parent = @parent);
-            UPDATE tree_struct SET display_order = (CASE WHEN @count = ord THEN ord - 1 ELSE ord + 1 END) WHERE id = @duplicatedId;
+            SET @duplicatedId = (SELECT id FROM tree_struct WHERE display_order = ord AND id != idx AND ((@parent is null and parent is null) OR parent = @parent) LIMIT 1);
+            UPDATE tree_struct SET display_order = @prevValue WHERE id = @duplicatedId;
         END;;
         DELIMITER ;');
     }
