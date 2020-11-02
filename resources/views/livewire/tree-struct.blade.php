cbb096c1c3e4e322d8fcef61269ca4d111df6846
Krzysztof Sternik <sternik15@gmail.com>
2020-11-02 01:20:18 +0100

    Make select input's error message red

--- resources/views/livewire/tree-struct.blade.php
+++ resources/views/livewire/tree-struct.blade.php
@@ -95,7 +95,7 @@
                                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
                                                 </div>
                                             </div>
-                                            @error('parent') <span class="text-danger error">{{ $message }}</span>@enderror
+                                            @error('parent') <p class="text-red-500 text-xs italic">{{ $message }}</p>@enderror
                                         </div>
                                     </div>
                                 </div>
